4363325eeb9aacfebfa5a971f701fc89ef4fa31c
borajoo <chocobora1@gmail.com>
2023-02-02 12:11:11 -0800

    Configure Amplify in App.tsx and Typescript fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return <p>Cleanedup React App</p>;
