e4820567f56b93e047093367426cb0394ed15ce5
annes <ahuynh69@calpoly.edu>
2023-02-04 12:44:17 -0800

    implemented react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
 
+import Temp from "./components/Temp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <p>Cleanedup React App</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* /, /login, /create-account, /forgot-password, /enter-code, /reset-password, /success */}
+        <Route path="/" element={<Temp />} />
+        <Route path="/login" element={<Temp />} />
+        <Route path="/create-account" element={<Temp />} />
+        <Route path="/forgot-password" element={<Temp />} />
+        <Route path="/enter-code" element={<Temp />} />
+        <Route path="/reset-password" element={<Temp />} />
+        <Route path="/success" element={<Temp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
